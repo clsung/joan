8c29fbded02a80e4787634e69554b5e22c03393b
cl_sung <cl_sung@htc.com>
2011-04-07 23:08:47 +0800

    - add self description

--- jobslist/models.py
+++ jobslist/models.py
@@ -4,9 +4,13 @@ class Company(models.Model):
     name = models.CharField(max_length=64)
     desc = models.CharField(max_length=256)
     url  = models.CharField(max_length=128)
+    def __unicode__(self):
+        return self.name
 
 class Job(models.Model):
     company = models.ForeignKey(Company)
     title   = models.CharField(max_length=24)
     desc    = models.CharField(max_length=512)
     salary  = models.CharField(max_length=12)
+    def __unicode__(self):
+        return self.title
