5bf98c20330e475397beed637630fce2df6a4dba
cl_sung <cl_sung@htc.com>
2011-04-08 23:39:34 +0800

    - change schema

--- jobslist/models.py
+++ jobslist/models.py
@@ -11,6 +11,8 @@ class Job(models.Model):
     company = models.ForeignKey(Company)
     title   = models.CharField(max_length=24)
     desc    = models.CharField(max_length=512)
-    salary  = models.CharField(max_length=12)
+    salary  = models.DecimalField(max_digits=8, decimal_places=0)
+    pub_date= models.DateTimeField('date published')
+    due_date= models.DateTimeField('due date')
     def __unicode__(self):
         return self.title
