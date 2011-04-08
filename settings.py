1b0624cd75b2b01249c5859a94ecb2258324038c
cl_sung <cl_sung@htc.com>
2011-04-08 23:44:16 +0800

    project name is joan

--- settings.py
+++ settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'jobs.urls'
+ROOT_URLCONF = 'joan.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
