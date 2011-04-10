13b7de087dda6320410e89f539e9d650842cfc93
cl_sung <cl_sung@htc.com>
2011-04-10 23:15:50 +0800

    - prepare to TDD

--- jobslist/tests.py
+++ jobslist/tests.py
@@ -6,13 +6,20 @@ Replace these with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from jobslist.models import *
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class CompanyTest(TestCase):
+    def setUp(self):
+        self.yahoo = Company.objects.create (name="yahoo",
+                desc = "The decsending company", 
+                url = "yahoo.com")
+        self.google = Company.objects.create (name="google",
+                desc = "The Evil company who claims don't do evil",
+                url = "google.com")
+
+    def testName(self):
+        self.assertEqual(self.yahoo.name, "yahoo")
+        self.assertEqual(self.google.name, "google")
 
 __test__ = {"doctest": """
 Another way to test that 1 + 1 is equal to 2.
